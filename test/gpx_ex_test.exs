b3cf9affa580604c5b3f7d6a0d4e6b4f7e49504c
Kacper Golinski <kacper.golinski@gmail.com>
2020-03-06 20:18:13 +0100

    add test gpx file

--- test/gpx_ex_test.exs
+++ test/gpx_ex_test.exs
@@ -1,4 +1,14 @@
 defmodule GpxExTest do
   use ExUnit.Case
   doctest GpxEx
+
+  import SweetXml
+
+  test "fooling around" do
+    {:ok, gpx} = File.read("./test/gpx_files/gdynia-gdansk.gpx")
+
+    assert xpath(gpx, ~x"//trk"l) |> length() == 1
+    assert xpath(gpx, ~x"//trk[1]/trkseg"l) |> length() == 1
+    assert xpath(gpx, ~x"//trk[1]/trkseg[1]/trkpt"l) |> length() == 3994
+  end
 end

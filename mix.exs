a5dcb5ec5d9025caf02d09b31a81cb29ea135c67
Kacper Golinski <kacper.golinski@gmail.com>
2020-03-21 20:42:21 +0100

    bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GpxEx.MixProject do
   def project do
     [
       app: :gpx_ex,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()

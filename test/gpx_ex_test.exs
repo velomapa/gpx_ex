327a73de5b16875950070675475d3c10f841652d
Tyler Young <tyler@tylerayoung.com>
2022-06-01 11:03:51 -0500

    Catch errors produced by xmerl
    This wraps the parse step in some error handling logic, so that rather than crashing the calling process on parse error, we return `{:error, something}`.

--- test/gpx_ex_test.exs
+++ test/gpx_ex_test.exs
@@ -103,4 +103,31 @@ defmodule GpxExTest do
 
     assert {:ok, expected} == GpxEx.parse(gpx_doc)
   end
+
+  test "gracefully handles unexpected ends to XML" do
+    incomplete = """
+    <?xml version="1.0" encoding="UTF-8"?>
+    <gpx version="1.1" creator="Apple Health Export" xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">
+      <metadata>
+        <name>Sample GPX file</name>
+        <desc>Lorem ipsum</desc>
+        <time>2022-04-11T14:08:13Z</time>
+      </metadata>
+      <rte>
+    """
+
+    ExUnit.CaptureLog.capture_log(fn ->
+      assert {:error, :unexpected_end} == GpxEx.parse(incomplete)
+    end)
+  end
+
+  test "gracefully handles missing elements" do
+    missing_elements = """
+    <?xml version="1.0" encoding="UTF-8"?>
+    """
+
+    ExUnit.CaptureLog.capture_log(fn ->
+      assert {:error, :expected_element_start_tag} == GpxEx.parse(missing_elements)
+    end)
+  end
 end
